09bb96ca4b23cb86506fa1daf69b7790cbeb516d
MM <mykola.mykhaliuk@gmail.com>
2025-12-01 00:36:00 +0100

    feat: initialize Angular landing page project with core components and structure

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { HeroComponent } from './components/hero/hero';
+import { AboutComponent } from './components/about/about';
+import { SkillsComponent } from './components/skills/skills';
+import { ProjectsComponent } from './components/projects/projects';
+import { ContactComponent } from './components/contact/contact';
+import { FooterComponent } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('mm-landing');
+}
